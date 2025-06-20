f334b2d1f251580c519711a4ffc7252e0b1efa66
Likitha <developer@arminfoserve.com>
2025-06-20 14:50:34 +0530

    chore: add build script to install Chrome for Selenium

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install Chrome for Selenium
+apt-get update
+apt-get install -y wget gnupg2
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
