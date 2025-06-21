45d9f859e39ef05936809e3ee832869b8bc90616
Likitha <developer@arminfoserve.com>
2025-06-21 15:03:06 +0530

    Replace webdriver-manager with chromedriver-autoinstaller for Render compatibility

--- scraper_logic.py
+++ scraper_logic.py
@@ -1,4 +1,4 @@
-import undetected_chromedriver as uc
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -234,15 +234,14 @@ def calculate_remaining_days(end_date_str):
         return "N/A"
 
 def run_warranty_check(serial_number, product_number=None, eosl_data=eosl_data):
-    # Automatically download and install the correct ChromeDriver version
-    options = uc.ChromeOptions()
-    options.headless = True
+    chromedriver_autoinstaller.install()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = uc.Chrome(options=options)
-
-    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 7)
 
     try:
